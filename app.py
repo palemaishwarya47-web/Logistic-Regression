387723e16cb9920792d4714c881d58f8bd8dbbe0
palemaishwarya47-web <palemaishwarya47@gmail.com>
2026-05-18 22:10:21 +0530

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import math
 from sklearn.linear_model import LogisticRegression
